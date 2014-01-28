cd953c4c29aa9cd8dfd3a37bba2b8fd598f32c38
Eirik Ulvik <eirik.ulvik@gmail.com>
2014-01-28 13:03:31 +0100

    Added post support and async handling

--- node/router.js
+++ node/router.js
@@ -1,8 +1,11 @@
-function route(handle, pathname) {
+function route(handle, pathname, response, postData) {
 	if(typeof(handle[pathname]) === 'function') {
-		handle[pathname]();
+		handle[pathname](response, postData);
 	} else {
 		console.log("No request handler found for route: " + pathname);
+		response.writeHead(404, {"Content-Type" : "text/plain"});
+		response.write("404 Not Found");
+		response.end();
 	}
 }
 
--- node/simpleserver.js
+++ node/simpleserver.js
@@ -3,14 +3,22 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response) {
+		var postData = "";
+		request.setEncoding("utf8");
+		
 		var pathname = url.parse(request.url).pathname;
+		
 		console.log("Request for " + pathname + " received");
 
-		route(handle, pathname);
+		request.addListener("data", function(postDataChunk) { 
+			postData += postDataChunk;
+			console.log("Received POST data chunk '"+ postDataChunk + "'.");
+		});
 
-		response.writeHead(200, {"Content-Type" : "text/plain"});	
-		response.write("Hello World");
-		response.end();
+		request.addListener("end", function() { 
+			route(handle, pathname, response, postData);
+		});
+
 	}
 
 	http.createServer(onRequest).listen(8888);
