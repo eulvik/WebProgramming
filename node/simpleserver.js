f5a2e8a02eafc15922e9b15a806c739d3f08558a
Eirik Ulvik <eirik.ulvik@gmail.com>
2014-01-29 21:15:49 +0100

    Functioning file upload

--- node/simpleserver.js
+++ node/simpleserver.js
@@ -3,22 +3,12 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response) {
-		var postData = "";
-		request.setEncoding("utf8");
 		
 		var pathname = url.parse(request.url).pathname;
 		
 		console.log("Request for " + pathname + " received");
 
-		request.addListener("data", function(postDataChunk) { 
-			postData += postDataChunk;
-			console.log("Received POST data chunk '"+ postDataChunk + "'.");
-		});
-
-		request.addListener("end", function() { 
-			route(handle, pathname, response, postData);
-		});
-
+		route(handle, pathname, response, request);
 	}
 
 	http.createServer(onRequest).listen(8888);
