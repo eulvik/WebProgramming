6bc5abcb2d4bf830c31ebc465109ddab8a2cddde
Eirik Ulvik <eirik.ulvik@gmail.com>
2016-02-01 19:54:06 +0100

    Started with MVC

---
+++ TheWorldCode/Controllers/Web/AppController.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.AspNet.Mvc;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
