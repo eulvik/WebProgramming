eb0b5908c22c2145f574641348e92309141ef646
Eirik Ulvik <eirik.ulvik@gmail.com>
2016-02-12 16:00:15 +0100

    Started with bootstrap

--- TheWorldCode/wwwroot/js/site.js
+++ TheWorldCode/wwwroot/js/site.js
@@ -22,12 +22,15 @@
     
     var $sideBarAndWrapper = $("#sidebar,#wrapper");
     
+    var $icon = $("#sidebarToggle i.fa");
     $("#sidebarToggle").on("click", function() {
         $sideBarAndWrapper.toggleClass("hide-sidebar");
         if($sideBarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show Sidebar");
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
         } else {
-            $(this).text("Hide Sidebar");
+            $icon.addClass("fa-angle-left");
+            $icon.removeClass("fa-angle-right");
         }
     });
 })();
