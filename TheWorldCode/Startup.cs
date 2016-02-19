66892fcc544ee13b647c345d9457c8155c390ea0
Eirik Ulvik <eirik.ulvik@gmail.com>
2016-02-19 16:27:36 +0100

    Started working with entity framework 7

--- TheWorldCode/Startup.cs
+++ TheWorldCode/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.PlatformAbstractions;
 using TheWorld.Services;
+using TheWorld.Models;
 
 namespace TheWorldCode
 {
@@ -24,6 +25,9 @@ namespace TheWorldCode
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            
+            services.AddEntityFramework().AddSqlServer().AddDbContext<WorldContext>();
+            
         #if DEBUG
             services.AddScoped<IMailService, DebugMailService>();
         #else
