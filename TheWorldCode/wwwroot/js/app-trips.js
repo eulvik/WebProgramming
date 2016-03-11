732eeb08367167b5128846cf5647eb111d2d16fd
Eirik Ulvik <Eirik Ulvik>
2016-03-11 15:13:50 +0100

    Some work on angular routing

--- TheWorldCode/wwwroot/js/app-trips.js
+++ TheWorldCode/wwwroot/js/app-trips.js
@@ -1,5 +1,13 @@
 (function () {
     "use-strict";
     
-    angular.module("app-trips", ["simpleControls"]);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+            .config(function ($routeProvider) {
+                $routeProvider.when("/", {
+                    controller:"tripsController",
+                    controllerAs: "vm",
+                    templateUrl: "tripsView.html"
+                });
+                $routeProvider.otherwise({ redirectTo: "/"});
+            });
 })();
