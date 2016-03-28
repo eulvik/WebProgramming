05c053c92c2cb70de0668d3f1e95f10d5755047a
Eirik Ulvik <eirik.ulvik@gmail.com>
2016-03-28 19:34:57 +0200

    Working on gulp stuff

---
+++ TheWorldCode/gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+
+gulp.task("minify", function(){
+    return gulp.src("wwwroot/js/*.js")
+                .pipe(uglify())
+                .pipe(gulp.dest("wwwroot/lib/_app"))
+});
