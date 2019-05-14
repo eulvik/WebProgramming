9a73785baa831c467d418d6542da0ecc1f59b594
Eirik UlviK <eirik@bravonet.no>
2019-05-14 08:05:54 +0200

    Azure functions logging

---
+++ Azure/Logging/Common/Service.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Common
+{
+    public interface IService
+    {
+        void DoWork();
+    }
+
+    public class Service : IService
+    {
+        private readonly ILogger<Service> _logger;
+
+        public Service(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Service>();
+        }
+
+        public Service(ILogger<Service> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoWork()
+        {
+            _logger.LogInformation("Performing work!!!!!");
+        }
+    }
+}
---
+++ Azure/Logging/LoggerApp/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.WebJobs.Hosting;
+using Common;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+
+[assembly: WebJobsStartup(typeof(LoggerApp.Startup))]
+namespace LoggerApp
+{
+    public class Startup: IWebJobsStartup
+    {
+        public Startup()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.ApplicationInsights(TelemetryConfiguration.Active, TelemetryConverter.Traces)
+                .CreateLogger();
+        }
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.Services.AddScoped<IService, Service>();
+            builder.Services.AddLogging(loggingBuilder => { loggingBuilder.AddSerilog(dispose: true); });
+            builder.Services.BuildServiceProvider(true);
+        }
+    }
+}
